3940dc6a935dfaa98e67847cd11ffaceb202cf21
Jaclyn Barcal <cchaney@ChristohersMini.attlocal.net>
2023-10-19 11:23:30 -0500

    Changed connection string for Mongo DB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/national-parks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/national-parks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://parkit:goteamgo@parkit-bootcamp.bdckvne.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
