d11f67e52a3b4714411fb41ed124d5af6f7068ad
Christopher Chaney <cchaney@ChristohersMini.attlocal.net>
2023-10-19 17:53:12 -0500

    Removed public exposure of MongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/national-parks');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://parkit:goteamgo@parkit-bootcamp.bdckvne.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/national-parks');
 
 module.exports = mongoose.connection;
